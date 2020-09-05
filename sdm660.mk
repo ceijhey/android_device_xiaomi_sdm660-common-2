9e73c7e2e2709fc6f71f92169df0780b48c30d66
iamehsangh <ehsan.ghasab@gmail.com>
2020-09-05 20:27:16 +0000

    sdm660-common: Remove Snap And Snap Overlays

--- sdm660.mk
+++ sdm660.mk
@@ -34,7 +34,6 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
-    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap \
     $(LOCAL_PATH)/overlay-system
 
 # Soong namespaces
@@ -162,9 +161,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
 # Camera
-PRODUCT_PACKAGES += \
-    Snap
-
 PRODUCT_PACKAGES += \
     android.frameworks.bufferhub@1.0.vendor \
     android.hardware.camera.device@3.4 \
